e5eb305a9753eb94b2b256967cef3dcf2f91ff9e
Gustavo Guerino <gustavoguerino@hotmail.com>
2016-10-13 22:27:06 -0300

    Initial implementation of new ds-generator(Still not completed)
    Still not completed

--- iuga-algorithm/ds-generator/ds-generator.py
+++ iuga-algorithm/ds-generator/ds-generator.py
@@ -1,28 +1,16 @@
-import csv
-#   Configuration
-#
-#       First Point:
-#           latpick = Latitude Point
-#           longpick = Longitude Point
-#       Seccond Point:
-#           latdrop = Latitude Point
-#           longdrop = Longitude Point
-#       Others columns:
-#           Distance            4
-#           Hours pickup        1
-#           Passengers          3
-
+import csv, math
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
 latpick, longpick           = [6, 5]
 latdrop, longdrop           = [10, 9]
 Distance, Hour, Passenger   = [4, 1, 3]
-
-# if csv have headers line 'firstline = True' else 'firstline = False'
-biggerdistance, biggersimilarity, firstline =   [0, 0, True]
-
-#   Basic function
-#   Read date format and extract the hour
-
-# Placido considerei remover as linhas com informacoes erradas... fiz o try para corrigir algumas linhas vazias, porem nao acredito que seja o caminho correto.
+biggerdistance, biggersimilarity, firstline, countlines =   [0, 0, True, 0]
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
 def getHour(strdate):
     try:
         strdate = strdate.split()
@@ -30,50 +18,68 @@ def getHour(strdate):
     except(IndexError):
         return 0
 
-print("\n\nLoading CSV Dataset...")
-
-with open('arquivo.csv') as f1:
-    print("\n\nReading CSV to find reference values...\n\n")
-    for row in csv.reader(iter(f1.readline, '')):
-#   searching bigger similarity value and bigger distance
-        if(firstline == False): # Ignore header line
-            row[Distance] = float(row[Distance])
-            if(biggerdistance < row[Distance]):
-                biggerdistance = row[Distance]
-#   Similarity is the result of:
-#       hour + (distance * passengers)
-            similarity = getHour(row[Hour]) + (row[Distance] * int(row[Passenger]))
-            if(biggersimilarity < similarity):
-                biggersimilarity = similarity
-        else:
-            firstline = False
-#   Writing ds.csv
-print("Bigger distance: ", biggerdistance, "Bigger similarity: ", biggersimilarity)
-
-print("\n\nWriting ds.csv")
- # If True not read headers line
-firstline = True
-
-w = csv.writer(open("ds.csv", "wb"))
+def harvestine_distance(lat1, lng1, lat2, lng2):
+    try:
+    	dept_lat_rad = math.radians(lat1)
+    	dept_lng_rad = math.radians(lng1)
+    	arr_lat_rad = math.radians(lat2)
+    	arr_lng_rad = math.radians(lng2)
+    	earth_radius = 3963.1
+    	d = math.acos(math.cos(dept_lat_rad)*math.cos(dept_lng_rad)*math.cos(arr_lat_rad)*math.cos(arr_lng_rad) + math.cos(dept_lat_rad)*math.sin(dept_lng_rad)*math.cos(arr_lat_rad)*math.sin(arr_lng_rad) + math.sin(dept_lat_rad)*math.sin(arr_lat_rad)) * earth_radius
+    	return round(d,2)
+    except:
+        return 0
+        #       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+        #       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+        #       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+        #       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+countlines = -1
+dscsv = csv.writer(open("ds.csv", "wb"))
+index = csv.writer(open("index.csv", "wb"))
 with open('arquivo.csv') as f1:
     for row in csv.reader(iter(f1.readline, '')):
+        countlines += 1
         if(firstline == False): # Ignore header line
-            similarity = getHour(row[Hour]) + (float(row[Distance]) * int(row[Passenger]))
-            #    Plus 180 to remove negative numbers, and concatene with '_' to facility split after
-            #    I not found other way to convert latitude and longitude to unique number
-            pickuppoint = str(float(row[latpick]) + 180) + "_" + str(float(row[longpick])+180)
-            dropoffpoint = str(float(row[latdrop]) + 180) + "_" + str(float(row[longdrop])+180)
+            index.writerow([(countlines*2) - 1, row[latpick], row[longpick]])
+            index.writerow([(countlines*2), row[latdrop], row[longdrop]])
+            loopfirstline = True
+            loopcountlines = -1
+            with open('arquivo.csv') as f2:
+                for looprow in csv.reader(iter(f2.readline, '')):
+                    loopcountlines += 1
+                    if(loopfirstline == False):
+                        if(countlines < loopcountlines):
+                            try:
+                                partsimilarity =  (  (int(row[Passenger]) + int(looprow[Passenger])) + (getHour(row[Hour]) / getHour(looprow[Hour]))  )
+                            except:
+                                partsimilarity =  (  (int(row[Passenger]) + int(looprow[Passenger]))  )
+                            columnsid = [(countlines*2) - 1, (countlines*2) , (loopcountlines*2) - 1, (loopcountlines*2)]
+                            # Column 1 actual X Column 2 below
+                            distance = harvestine_distance(float(row[latpick]), float(row[longpick]), float(looprow[latdrop]), float(looprow[longdrop]))
+                            similarity = distance * partsimilarity
+                            dscsv.writerow([columnsid[0], columnsid[3], distance, similarity])
+                            # Column 1 actual X Column 1 below
+                            distance = harvestine_distance(float(row[latpick]), float(row[longpick]), float(looprow[latpick]), float(looprow[longpick]))
+                            similarity = distance * partsimilarity
+                            dscsv.writerow([columnsid[0], columnsid[2], distance, similarity])
+                            # Column 2 actual X Column 2 below
+                            distance = harvestine_distance(float(row[latdrop]), float(row[longdrop]), float(looprow[latpick]), float(looprow[longpick]))
+                            similarity = distance * partsimilarity
+                            dscsv.writerow([columnsid[1], columnsid[3], distance, similarity])
+                            # Column 2 actual X Column 1 below
+                            distance = harvestine_distance(float(row[latdrop]), float(row[longdrop]), float(looprow[latdrop]), float(looprow[longdrop]))
+                            similarity = distance * partsimilarity
+                            dscsv.writerow([columnsid[1], columnsid[2], distance, similarity])
+                    else:
+                        loopfirstline = False
 
-            #latpickparts = row[latpick].split(".")
-            #longpickparts = row[longpick].split(".")
-            #latdropparts = row[latdrop].split(".")
-            #longdropparts = row[longdrop].split(".")
-            #pickuppoint = str(int(latpickparts[0])+180) + str(int(longpickparts[0]) + 180)
-            #dropoffpoint = str(int(latdropparts[0]) +180) + str(int(longdropparts[0]) + 180)
-            #pickuppoint = int(pickuppoint)
-            #dropoffpoint = int(dropoffpoint)
-
-            w.writerow([pickuppoint, dropoffpoint, float(row[Distance])/biggerdistance, similarity/biggersimilarity])
+            loopfirstline = True
+            f2.close()
         else:
             firstline = False
-print("\n\nFinished!")
+f1.close()
+
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
+#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!#       NOT FINISHED!
